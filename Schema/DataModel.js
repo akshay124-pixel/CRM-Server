6b9f11adb4a53b563787eb3d3b0c7bd50a1fd16b
Akshay Pratap Singh <akshay124-pixel@example.com>
2025-09-06 18:39:31 +0530

    Done

--- Schema/DataModel.js
+++ Schema/DataModel.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   specification: { type: String, required: true, trim: true },
   size: { type: String, required: true, trim: true },
-  quantity: { type: Number, required: true, min: 1 },
+  quantity: { type: Number, required: true },
 });
 
 const historySchema = new mongoose.Schema({
